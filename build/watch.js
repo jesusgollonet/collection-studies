614d78584e9df683fe77f1cf555aa64e89bf4e75
Jesus Gollonet <jesusgollonet@gmail.com>
2016-04-27 10:51:33 +0200

    Make watch compile file on start

--- build/watch.js
+++ build/watch.js
@@ -1,12 +1,18 @@
 var chokidar = require('chokidar');
 var exec = require('child_process').exec;
 
+function compileElmFile(){
+    var cmd = 'pwd && (cd elm && elm-make Main.elm   --output=../public/js/elm.js)';
+    exec(cmd, function(error, stdout, stderr) {
+        console.log(error, stdout, stderr);
+        // command output is in stdout
+    });
+}
 // One-liner for current directory, ignores .dotfiles
 chokidar.watch('elm/Main.elm', {ignored: /[\/\\]\./}).on('change', (path) => {
-  console.log(path, 'changed');
-  var cmd = 'pwd && (cd elm && elm-make Main.elm   --output=../public/js/elm.js)';
-  exec(cmd, function(error, stdout, stderr) {
-      console.log(error, stdout, stderr);
-      // command output is in stdout
-  });
+    console.log(path, 'changed');
+    compileElmFile();
 });
+
+compileElmFile();
+
